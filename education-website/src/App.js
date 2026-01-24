6d2ba65756cf8c07354d7e4ccd120b23afbbc4e0
임현빈 <imhyeonbin@imhyeonbin-ui-MacBookPro.local>
2026-01-24 17:05:22 +0900

    fix: import React to resolve compilation error

--- education-website/src/App.js
+++ education-website/src/App.js
@@ -1,4 +1,5 @@
 // Initializing App
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Location from './components/Location/Location';
